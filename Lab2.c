043b0c795c7ffe7cd0dbe0904505a3bd4c89ca59
Vincent <vsaw30@gmail.com>
2020-02-04 16:45:20 -0800

    Added Lab2a

--- Lab2.c
+++ Lab2.c
@@ -4,3 +4,6 @@ int main(void)                          /* this is a function */
     printf("My first C program\n");     /* this is a statement */
     return 0;
 }
+
+//gcc -o Lab2.exeLab2.c
+//./Lab2.exe
