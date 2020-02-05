4b9da18a94d7bc5f80f40c2977098ffdfb279ae7
vsawce <vsaw30@gmail.com>
2020-02-04 21:32:12 -0800

    Removed old remaining files

--- Lab2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>                      /* this is a directive */
-int main(void)                          /* this is a function */
-{
-    printf("My first C program\n");     /* this is a statement */
-    return 0;
-}
-
-//gcc -o Lab2.exeLab2.c
-//./Lab2.exe
